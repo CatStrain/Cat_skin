f58b4191d289cc0a29396ff3e89438fa88b03e59
Darío Urbina <urbiname@usc.edu>
2020-07-24 17:26:21 -0700

    working on simplifying code

--- valero_receive_data_from_arduino.m
+++ valero_receive_data_from_arduino.m
@@ -11,7 +11,8 @@ try                     %this is used to close the remaining portal and files
     fclose(fileID);
 end
 %%
-clear all;clc;  % clear all variables
+%clear all;
+clc;  % clear all variables
 %%
 
 s = serial('COM5');  %port
@@ -22,97 +23,99 @@ fileID = fopen('test_10.txt','w');
 strain_gauge_lecture = 160; %read 200 numbers from arduino. can be adjusted. 
                             %DUM: 200 times SG will be read
                             %since we have 8 Strain Gauges, this needs to
-                            %be a multiple of the Strain Gauge number 
+                            %be a multiple of the Strain Gauge number
+                            
+strain_gauges = 8;
+total_batches = strain_gauge_lecture/strain_gauges;
 passo = 1;
 
 batch = 1;
 
 t = 1;
-% x1 = 0;   %8 signals representing 8 strai gauges 
-% x2 = 0;   %...
-% x3 = 0;
-% x4 = 0;
-% x5 = 0;
-% x6 = 0;
-% x7 = 0;
-% x8 = 0;
+
 
 i=1;
 %m=zeros(1,1000); %counter
 
-data = zeros (strain_gauge_lecture/8,8);
+data_2 = zeros (strain_gauge_lecture/8,8);
 
-while(t < strain_gauge_lecture)
-    b = str2num(fgetl(s)); %takes one line from arduino
-    
-    if (mod(i,8)==1)        %DUM ## What is "mod"?
-        %x1 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,1) = b;
+% while(t < strain_gauge_lecture)
+%     b = str2num(fgetl(s)); %takes one line from arduino
+%     
+    for data_rows = 1: total_batches
+        b = str2num(fgetl(s))
+        for sg = 1:strain_gauges
+%             if (mod(i,8)==sg)
+%                 fprintf(fileID,'%d ',b);
+                if (b~=0)
+                    data_2(data_rows,sg) = b;
+                end
+%             end
         end
-    elseif (mod(i,8)==2)
-        %x2 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,2) = b;
-        end
-    elseif (mod(i,8)==3)
-        %x3 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,3) = b;
-        end
-    elseif (mod(i,8)==4)
-        %x4 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,4) = b;
-        end
-    elseif (mod(i,8)==5)
-        %x5 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,5) = b;
-        end
-    elseif (mod(i,8)==6)
-        %x6 = b;
-        fprintf(fileID,'%d ',b); 
-        if (b~=0)
-            data(batch,6) = b;
-        end
-    elseif (mod(i,8)==7)
-        %x7 = b;
-        fprintf(fileID,'%d ',b);
-        if (b~=0)
-            data(batch,7) = b;
-        end
-    else
-        %x8 = b; 
-        fprintf(fileID,'%d\n',b);
-        if (b~=0)
-            data(batch,8) = b;
-            batch = batch + 1;
-        else 
-            batch = batch + 1;
-        end
-    %drawnow;
     end
-    i=i+1;                
-    t = t+passo;
     
-end
+    
+%     
+%     if (mod(i,8)==1)        %DUM ## What is "mod"?
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,1) = b;
+%         end
+%     elseif (mod(i,8)==2)
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,2) = b;
+%         end
+%     elseif (mod(i,8)==3)
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,3) = b;
+%         end
+%     elseif (mod(i,8)==4)
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,4) = b;
+%         end
+%     elseif (mod(i,8)==5)
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,5) = b;
+%         end
+%     elseif (mod(i,8)==6)
+%         fprintf(fileID,'%d ',b); 
+%         if (b~=0)
+%             data(batch,6) = b;
+%         end
+%     elseif (mod(i,8)==7)
+%         fprintf(fileID,'%d ',b);
+%         if (b~=0)
+%             data(batch,7) = b;
+%         end
+%     else
+%         fprintf(fileID,'%d\n',b);
+%         if (b~=0)
+%             data(batch,8) = b;
+%             batch = batch + 1;
+%         else 
+%             batch = batch + 1;
+%         end
+%     %drawnow;
+%     end
+%     i=i+1;                
+     t = t+passo;
+    
+% end
 fclose(s);
 fclose(fileID);
 
 figure()
  
-plot(data(:,1));
-hold on 
-plot(data(:,2));
-hold on
-plot(data(:,3));
-hold on 
-plot(data(:,4));
-hold on
+% plot(data(:,1));
+% hold on 
+% plot(data(:,2));
+% hold on
+% plot(data(:,3));
+% hold on 
+% plot(data(:,4));
+% hold on
 
