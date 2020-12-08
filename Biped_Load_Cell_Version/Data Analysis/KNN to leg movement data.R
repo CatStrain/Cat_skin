5c0aa9de802f48d856d87f3940ffc46b14f5df6f
Darío Urbina <urbiname@usc.edu>
2020-12-07 17:09:12 -0800

    Kmeans

--- Biped_Load_Cell_Version/Data Analysis/KNN to leg movement data.R
+++ Biped_Load_Cell_Version/Data Analysis/KNN to leg movement data.R
@@ -2,49 +2,12 @@
 rm(list = ls()); # clear workspace variables
 cat("\014") # it means ctrl+L. clear window
 
-zmp_posotion <- 1
-flag_distance <- 25
-distance <- 1
+load("simple_zmp_locations_exp_1.Rda")
+load("simple_zmp_locations_exp_2.Rda")
+load("simple_zmp_locations_exp_3.Rda")
+load("simple_zmp_locations_exp_4.Rda")
 
-mypath_1 <- "~/Github/Cat_skin/Biped_Load_Cell_Version/Data Analysis/CSV files/test_120420_1.csv"
-zmp_locations_exp_1.data <- read.csv(mypath_1)
-
-simple_zmp_locations_exp_1.data <- as.data.frame(matrix(0, ncol = 5, nrow = (nrow(zmp_locations_exp_1.data)/25)))
-simple_zmp_row <-1
-
-x = replicate(nrow(zmp_locations_exp_1.data), 0)
-
-for (i in 1:nrow(zmp_locations_exp_1.data)) {         # Going through all the rows
-  zmp_locations_exp_1.data[i,5] <- zmp_posotion
-  if(distance == flag_distance){                      # To determine change of position label
-    zmp_posotion <- zmp_posotion+1
-    if (zmp_posotion == 10){
-      zmp_posotion <- 1
-    }
-  }
-  if(distance == (12)){                   #To pick only one line of data recorded per position
-    for (j in 1:ncol(zmp_locations_exp_1.data)){
-      simple_zmp_locations_exp_1.data[simple_zmp_row,j] <- zmp_locations_exp_1.data[i,j]
-    }
-    simple_zmp_row <- simple_zmp_row+1
-  }
-  distance <- distance+1
-  if (distance == 26){
-    distance <- 1
-  }
-    
-}
-
-newheaders <- c("LC_1", "LC_2", "LC_3", "LC_4","ZMP_location")
-colnames(simple_zmp_locations_exp_1.data) <- newheaders
-
-save(simple_zmp_locations_exp_1.data,file="simple_zmp_locations_exp_1.Rda")
-
-#zmp_locations_exp_1.data$label <- c(x)
-
-
-#zmp_locations_exp_1.data$label=label
-
+#View(simple_zmp_locations_exp_2.data)
 
 ##################################################################################################
 
@@ -52,19 +15,19 @@ save(simple_zmp_locations_exp_1.data,file="simple_zmp_locations_exp_1.Rda")
 
 
 #Separate class and data
-iris.class <- iris.data[,c("species")] #Actual classes
-iris.data <- iris.data[,c("sepal_length","sepal_width","petal_length","petal_width")]
+zmp.class <- simple_zmp_locations_exp_2.data[,c("ZMP_location")] #Actual classes
+simple_zmp_locations_exp_2.data <- simple_zmp_locations_exp_2.data[,c("LC_1","LC_2","LC_3","LC_4")]
 
-head(iris.data)
-#View(iris.data)
+head(simple_zmp_locations_exp_2.data)
+#View(simple_zmp_locations_exp_2.data)
 
 par(mfrow=c(2,3), mar=c(5,4,2,2))
-plot(iris.data[c("sepal_length","sepal_width")])# Plot to see how Sepal.Length and Sepal.Width data points have been distributed in clusters
-plot(iris.data[c("petal_length","petal_width")])# Plot to see how Petal.Length and Petal.Width data points have been distributed in clusters
-plot(iris.data[c("sepal_length","petal_length")])
-plot(iris.data[c("sepal_length","petal_width")])
-plot(iris.data[c("sepal_width","petal_length")])
-plot(iris.data[c("sepal_width","petal_width")])
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_2")])# Plot to see how LC_1 and LC_2 data points have been distributed in clusters
+plot(simple_zmp_locations_exp_2.data[c("LC_3","LC_4")])
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_3")])
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_4")])
+plot(simple_zmp_locations_exp_2.data[c("LC_2","LC_3")])
+plot(simple_zmp_locations_exp_2.data[c("LC_2","LC_4")])
 
 #Example visualize the data first on two dimension
 
@@ -74,16 +37,16 @@ normalize <- function(x){
 }
 
 #Normalization
-iris.data$sepal_length <- normalize(iris.data$sepal_length)
-iris.data$sepal_width <- normalize(iris.data$sepal_width)
-iris.data$petal_length <- normalize(iris.data$petal_length)
-iris.data$petal_width <- normalize(iris.data$petal_width)
-head(iris.data) #normalize the values to be within (0-1)
+simple_zmp_locations_exp_2.data$LC_1 <- normalize(simple_zmp_locations_exp_2.data$LC_1)
+simple_zmp_locations_exp_2.data$LC_2 <- normalize(simple_zmp_locations_exp_2.data$LC_2)
+simple_zmp_locations_exp_2.data$LC_3 <- normalize(simple_zmp_locations_exp_2.data$LC_3)
+simple_zmp_locations_exp_2.data$LC_4 <- normalize(simple_zmp_locations_exp_2.data$LC_4)
+head(simple_zmp_locations_exp_2.data) #normalize the values to be within (0-1)
 
-summary(iris.data)  #Summary of normalized iris.data 
+summary(simple_zmp_locations_exp_2.data)  #Summary of normalized simple_zmp_locations_exp_2.data 
 
-k <- 3 #No. clusters
-result<- kmeans(iris.data,k,iter.max=10,nstart=1)
+k <- 9 #No. clusters
+result<- kmeans(simple_zmp_locations_exp_2.data,k,iter.max=10,nstart=1)
 str(result) #Compactly Display the Structure of an Arbitrary R Object
 result$size
 result$centers #4 dimensions
@@ -92,31 +55,31 @@ result$cluster
 #Plot results, use scatter plot
 # 4 attributes, 6 combinations
 par(mfrow=c(2,3), mar=c(5,4,2,2))
-plot(iris.data[c("sepal_length","sepal_width")], col=result$cluster)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed in clusters
-plot(iris.data[c("petal_length","petal_width")], col=result$cluster)# Plot to see how Petal.Length and Petal.Width data points have been distributed in clusters
-plot(iris.data[c("sepal_length","petal_length")], col=result$cluster)
-plot(iris.data[c("sepal_length","petal_width")], col=result$cluster)
-plot(iris.data[c("sepal_width","petal_length")], col=result$cluster)
-plot(iris.data[c("sepal_width","petal_width")], col=result$cluster)
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_2")], col=result$cluster)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed in clusters
+plot(simple_zmp_locations_exp_2.data[c("LC_3","LC_4")], col=result$cluster)# Plot to see how Petal.Length and Petal.Width data points have been distributed in clusters
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_3")], col=result$cluster)
+plot(simple_zmp_locations_exp_2.data[c("LC_1","LC_4")], col=result$cluster)
+plot(simple_zmp_locations_exp_2.data[c("LC_2","LC_3")], col=result$cluster)
+plot(simple_zmp_locations_exp_2.data[c("LC_2","LC_4")], col=result$cluster)
 
 
-# Choose the number of clusters k= 1-5
-k.values <- 1:10
-#Do for loops for different k-values
-tot.withinss.k <- rep(0,length(k.values))
-count <-1
-for (val in k.values) {
-  result<-kmeans(iris.data,val,iter.max=10,nstart=1)
-  
-  tot.withinss.k[count]<-result$tot.withinss #calculating the within sum of sqrs
-  count<-count+1
-}
-
-graphics.off() # close all plots
-plot(k.values, tot.withinss.k,
-     type="b", pch = 19, frame = FALSE, 
-     xlab="Number of clusters K",
-     ylab="Total within-clusters sum of squares")
+# # Choose the number of clusters k= 1-5
+# k.values <- 1:10
+# #Do for loops for different k-values
+# tot.withinss.k <- rep(0,length(k.values))
+# count <-1
+# for (val in k.values) {
+#   result<-kmeans(simple_zmp_locations_exp_2.data,val,iter.max=10,nstart=1)
+#   
+#   tot.withinss.k[count]<-result$tot.withinss #calculating the within sum of sqrs
+#   count<-count+1
+# }
+# 
+# graphics.off() # close all plots
+# plot(k.values, tot.withinss.k,
+#      type="b", pch = 19, frame = FALSE, 
+#      xlab="Number of clusters K",
+#      ylab="Total within-clusters sum of squares")
 
 #Accuracy (Correction)
 
@@ -132,10 +95,10 @@ id.test <- rnum[131:150] #Choose test set
 
 
 #Training & test sets
-iris.train<- iris.data[id.training,]
-iris.train.target<- iris.class[id.training]
-iris.test<- iris.data[id.test,]
-iris.test.target<- iris.class[id.test]
+zmp.train<- simple_zmp_locations_exp_2.data[id.training,]
+zmp.train.target<- zmp.class[id.training]
+zmp.test<- simple_zmp_locations_exp_2.data[id.test,]
+zmp.test.target<- zmp.class[id.test]
 
 
 #Use knn function
@@ -147,10 +110,10 @@ library(class)
 #.rs.restartR()
 
 
-model1<- knn(train=iris.train, test=iris.test, cl=iris.train.target, k=16)
+model1<- knn(train=zmp.train, test=zmp.test, cl=zmp.train.target, k=16)
 
 #Model accuracy : create the confucion matrix
-tb <- table(iris.test.target, model1)
+tb <- table(zmp.test.target, model1)
 tb
 accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
 accuracy(tb)
@@ -162,8 +125,8 @@ k.neighbor.values <- c(2,4,6,8,10,12,14,16,18,20) #which is better?
 accuracy.k <- rep(0,length(k.neighbor.values))
 count <-1
 for (val in k.neighbor.values) {
-  model1<- knn(train=iris.train, test=iris.test, cl=iris.train.target, k=val)
-  tb <- table(iris.test.target, model1)
+  model1<- knn(train=zmp.train, test=zmp.test, cl=zmp.train.target, k=val)
+  tb <- table(zmp.test.target, model1)
   
   accuracy.k[count]<-accuracy(tb)
   count<-count+1
