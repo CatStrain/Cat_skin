1744d719c6243389c5402d517178b6900b7505d2
Darío Urbina <urbiname@usc.edu>
2020-12-07 16:15:12 -0800

    simple data file created

--- Biped_Load_Cell_Version/Data Analysis/KNN to leg movement data.R
+++ Biped_Load_Cell_Version/Data Analysis/KNN to leg movement data.R
@@ -9,16 +9,24 @@ distance <- 1
 mypath_1 <- "~/Github/Cat_skin/Biped_Load_Cell_Version/Data Analysis/CSV files/test_120420_1.csv"
 zmp_locations_exp_1.data <- read.csv(mypath_1)
 
+simple_zmp_locations_exp_1.data <- as.data.frame(matrix(0, ncol = 5, nrow = (nrow(zmp_locations_exp_1.data)/25)))
+simple_zmp_row <-1
 
 x = replicate(nrow(zmp_locations_exp_1.data), 0)
 
-for (i in 1:nrow(zmp_locations_exp_1.data)) {
+for (i in 1:nrow(zmp_locations_exp_1.data)) {         # Going through all the rows
   zmp_locations_exp_1.data[i,5] <- zmp_posotion
-  if(distance == flag_distance){
+  if(distance == flag_distance){                      # To determine change of position label
     zmp_posotion <- zmp_posotion+1
     if (zmp_posotion == 10){
       zmp_posotion <- 1
     }
+  }
+  if(distance == (12)){                   #To pick only one line of data recorded per position
+    for (j in 1:ncol(zmp_locations_exp_1.data)){
+      simple_zmp_locations_exp_1.data[simple_zmp_row,j] <- zmp_locations_exp_1.data[i,j]
+    }
+    simple_zmp_row <- simple_zmp_row+1
   }
   distance <- distance+1
   if (distance == 26){
@@ -28,7 +36,7 @@ for (i in 1:nrow(zmp_locations_exp_1.data)) {
 }
 
 newheaders <- c("LC_1", "LC_2", "LC_3", "LC_4","ZMP_location")
-colnames(zmp_locations_exp_1.data) <- newheaders
+colnames(simple_zmp_locations_exp_1.data) <- newheaders
 
 #zmp_locations_exp_1.data$label <- c(x)
 
