b02728dcd71afc6a17929791c460212a92a0ba10
Darío Urbina <urbiname@usc.edu>
2021-01-18 11:21:26 -0800

    changes to analyse data

--- Code/Data Analysis/main.R
+++ Code/Data Analysis/main.R
@@ -6,7 +6,7 @@ library(class)
 library(ggplot2)
 library(caret)                # "R package that provides general interface to +- 150 ML alg"
 library("lattice")
-mypath_1 <- "C:/Users/dario/Documents/Github/Cat_skin/Code/Data Analysis/test_121720_2_elasticBands_blackMass_randomF.txt"   
+mypath_1 <- "C:/Users/dario/Documents/Github/Cat_skin/Data/Backup/ZMP_9points_ElasticBands(Mat1)_and_Spring_COMBINATION_PLANT/test_0111_800g_lessstiff+black_random_3.txt"   
 data.raw <- read.csv(mypath_1)                                                   # Creating data frame from data csv file
 #DOWNSAMPLING DATA:
 zmp_posotions_all = rep(c(1:9), times = ceiling(nrow(data.raw)/(25*9)))          #  generating label patterns
@@ -27,3 +27,19 @@ fit <- train(ZMP_location ~ .,
 print(fit)                                            # print results    
 print(confusionMatrix(fit))
 levelplot(confusionMatrix(fit)$table)                 # print the confusion matrix
+
+#mean(data.downsampled$LC_1)
+#mean(data.downsampled$LC_2)
+#mean(data.downsampled$LC_3)
+#mean(data.downsampled$LC_4)
+
+min_max_norm <- function(x) {
+   (x - min(x)) / (max(x) - min(x))
+}
+
+data.downsampled_norm <- as.data.frame(lapply(data.downsampled[1:4], min_max_norm))
+
+sd(data.downsampled_norm$LC_1)
+sd(data.downsampled_norm$LC_2)
+sd(data.downsampled_norm$LC_3)
+sd(data.downsampled_norm$LC_4)
--- Code/data_processing_prep_for_ML.m
+++ Code/data_processing_prep_for_ML.m
@@ -9,16 +9,23 @@ zmp_position = 1;
 zmp_position_limit = 9;
 row_l_data = 1;
 
-data = importdata('C:\Users\dario\Documents\Github\Cat_skin\Biped_Load_Cell_Version\Data Analysis\Backup\ZMP_9points_SpringPlant\test_122120_2_SpringPlant_blackMass.txt');
-labeled_and_labeled_data = zeros (floor(size(data,1)/flag_distace), size(data,2)+1);
+data = importdata('C:\Users\dario\Google Drive\USC\Projects\Biped\Figure 2_Raw Signals\test_122220_3_SpringPlant_ProximalAttachment_blackMass.txt');
+%labeled_and_labeled_data = zeros (floor(size(data,1)/flag_distace), size(data,2)+1);
 %csv_file_name = 'test_121620_1_Loadcells_blackMass' 
 %%
+%data(:,4) = data(:,4)+250;
+
 figure()
 for sensors = 1 : number_of_sensors
-    plot(data(:,sensors));
+    plot(data(:,sensors),'LineWidth',.1);
     hold on 
 end
-legend('Elasctic Band 1','2','3','4')   
+legend('Load Cell:  1','                  2','                  3','                  4', 'FontSize',12)   
+%legend('Skin Sensor:  1','                      2','                      3','                      4', 'FontSize',12)   
+xlabel('Sample #', 'FontSize',15) 
+ylabel('Sensor Reading','FontSize',15)
+
+%set(gca,'FontSize',15)
 
 %% To reverse seonsr calibration (to pair lower limit of sensors with 0)
 % data(:,1) = data(:,1) - sensor_1_offset;
@@ -26,32 +33,32 @@ legend('Elasctic Band 1','2','3','4')
 % data(:,3) = data(:,3) - sensor_3_offset;
 % data(:,4) = data(:,4) - sensor_4_offset;
 
-
-figure()
-for sensors = 1 : number_of_sensors
-    plot(data(:,sensors));
-    hold on 
-end
-legend('Elasctic Band 1','2','3','4')
-
-%% Simpifying data matrix: only taking one value per step, adding flags, and creating csv file
-
-%zeros(length(data),1);
-row_l_data = 1;
-for i = 1 : length(labeled_and_labeled_data)
-    labeled_and_labeled_data(row_l_data,:) = [data(((row_l_data*flag_distace)-12),:), zmp_position];
-    zmp_position = zmp_position+1;
-    if zmp_position == (zmp_position_limit)
-         zmp_posotion = 1;
-    end
-    row_l_data = row_l_data + 1;
-end
-
-figure()
-for sensors = 1 : number_of_sensors
-    plot(labeled_and_labeled_data(:,sensors));
-    hold on 
-end
-legend('Elasctic Band 1','2','3','4')
-
-%csvwrite(csv_file_name,labeled_and_labeled_data)
+% 
+% figure()
+% for sensors = 1 : number_of_sensors
+%     plot(data(:,sensors));
+%     hold on 
+% end
+% legend('Elasctic Band 1','2','3','4')
+% 
+% %% Simpifying data matrix: only taking one value per step, adding flags, and creating csv file
+% 
+% %zeros(length(data),1);
+% row_l_data = 1;
+% for i = 1 : length(labeled_and_labeled_data)
+%     labeled_and_labeled_data(row_l_data,:) = [data(((row_l_data*flag_distace)-12),:), zmp_position];
+%     zmp_position = zmp_position+1;
+%     if zmp_position == (zmp_position_limit)
+%          zmp_posotion = 1;
+%     end
+%     row_l_data = row_l_data + 1;
+% end
+% 
+% figure()
+% for sensors = 1 : number_of_sensors
+%     plot(labeled_and_labeled_data(:,sensors));
+%     hold on 
+% end
+% legend('Elasctic Band 1','2','3','4')
+% 
+% %csvwrite(csv_file_name,labeled_and_labeled_data)
