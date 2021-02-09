dc8b77705f41c39573f7166af18b2929b8cecf55
Darío Urbina <urbiname@usc.edu>
2021-02-09 09:22:00 -0800

    time delay is given by Arduino, not by MATLAB, time compensation function should be part of Arduino code

--- Code/data_acquisition.m
+++ Code/data_acquisition.m
@@ -20,28 +20,13 @@ s_1=serial('COM6','baudrate',9600);
 %Biped
 try
     fopen(s_1);
-catch err
+catch error
     fclose(instrfind);
     error('Make sure you select the correct COM6 Port where the Arduino is connected.');
 end
 
-%% old:
-% try                     %this is used to close the remaining portal and files
-%     fclose(s_1);          
-%     fclose(s_2); 
-% end
-%%%
-% clear all;
-% clc;  % clear all variables
-%%%
-% 
-% s_1 = serial('COM5');  
-% set(s_1,'BaudRate',9600);  
-% fopen(s_1);  
-
-%%
 sensor_lecture = 25000; 
-number_of_sensors = 4;       
+number_of_sensors = 8;       
 lecture_line = sensor_lecture/number_of_sensors;
 
 data_biped = zeros (lecture_line,number_of_sensors);
@@ -50,8 +35,8 @@ batch_size = 25;
 batch_line = 1;
 CoP_cue = 1;
 biggest_CoP_value = 9;
-
-transition_waiting_time = 3;        %To have control over the batch transition time, specially to coordinate two DAQ instances 
+waith_time=0;
+transition_waiting_time = 20;        %To have control over the batch transition time, specially to coordinate two DAQ instances 
 
 for i = 1: lecture_line             %to show flags every 25 lines of data file reading (one barch, 25 lines)
     if batch_line == batch_size
@@ -73,18 +58,20 @@ for data_line = 1: lecture_line
         end
     end
     if batch_line_array(data_line)==1                  %there are 25 lines per batch, only enter this when value of a line is 1
+        fprintf ('25 data lines recorded...')
         if CoP_cue == biggest_CoP_value
             CoP_cue = 0;                   %
         end
-        while (a < transition_waiting_time)         %This loop won't allow the code to continue until the transition_waiting_time condition is met
+        while (waith_time < transition_waiting_time)         %This loop won't allow the code to continue until the transition_waiting_time condition is met
             referece_clock = clock;
-            a = (referece_clock(5)*60 + referece_clock(6)) - (batch_record_start_time(5)*60 + batch_record_start_time(6));
+            waith_time = (referece_clock(5)*60 + referece_clock(6)) - (batch_record_start_time(5)*60 + batch_record_start_time(6));
             %fprintf("waiting for the difference to be bigger than 60...\n") 
         end
         batch_record_start_time = clock;
         CoP_cue = CoP_cue +1        
     end
 end
+fclose(s_1);
 record_end_time = clock;
 
 data_biped=data_biped';
