7611817da13d7e638ebecc8e2b8bd1a606e50244
Darío Urbina <urbiname@usc.edu>
2021-02-24 19:18:12 -0800

    adding correct terminlogy

--- Code/Data Analysis/main.R
+++ Code/Data Analysis/main.R
@@ -6,24 +6,15 @@ library(class)
 library(ggplot2)
 library(caret)                # "R package that provides general interface to +- 150 ML alg"
 library("lattice")
-mypath_1 <- "C:/Users/dario/Documents/Github/Cat_skin/Data/Backup/ZMP_16points_Skin_BlackMass/test_0219_single_leg_random_3.txt"   
+mypath_1 <- "C:/Users/dario/Documents/Github/Cat_skin/Data/Backup/zmp_16points_Skin_BlackMass/test_0219_single_leg_random_3.txt"   
 data.raw <- read.csv(mypath_1)                                                   # Creating data frame from data csv file
-
-########
-
-#setwd("~/Github/Cat_skin/Data/Backup/ZMP_9points_SkinMat2_BlackMass")
-#load("constant_force_2_skin.Rda")
-
-#Change this line to analyze different files:
-#data.raw <-constant_force_2_skin.data
-########
 
 
 #DOWNSAMPLING DATA:
-zmp_posotions_all = rep(c(1:16), times = ceiling(nrow(data.raw)/(25*9)))          #  generating label patterns
+CoP_posotions_all = rep(c(1:16), times = ceiling(nrow(data.raw)/(25*9)))         #  generating label patterns
 data.downsampled = data.raw[seq(12,nrow(data.raw),25),]                          # down sampling data seq.int(from, to, by, length.out, along.with, ...)
-data.downsampled[,5] = zmp_posotions_all[1:250]                                  # selecting labels to fit the data size
-newheaders <- c("LC_1", "LC_2", "LC_3", "LC_4","ZMP_location")                   # To Add headers to the downsampled data
+data.downsampled[,5] = CoP_posotions_all[1:250]                                  # selecting labels to fit the data size
+newheaders <- c("LC_1", "LC_2", "LC_3", "LC_4","CoP_location")                   # To Add headers to the downsampled data
 colnames(data.downsampled) <- newheaders
 
 trans_1 <- preProcess(data.downsampled, method = c("range"))
@@ -31,9 +22,9 @@ data.downsampled = predict(trans_1, data.downsampled[,1:5])
 
 # KNN
 set.seed(99)                                                                     # required to reproduce the results
-data.downsampled['ZMP_location'] = factor(data.downsampled[,'ZMP_location'])
+data.downsampled['CoP_location'] = factor(data.downsampled[,'CoP_location'])
 trControl <- trainControl(method  = "cv", number  = 5)                           # 5 fold Cross-Validation
-fit <- train(ZMP_location ~ .,
+fit <- train(CoP_location ~ .,
       method     = "knn",
       tuneGrid   = expand.grid(k = 1:20),
       trControl  = trControl,
@@ -48,6 +39,8 @@ min_max_norm <- function(x) {
    (x - min(x)) / (max(x) - min(x))
 }
 
+#####
+#Extra analeses 
 data.downsampled_norm <- as.data.frame(lapply(data.downsampled[1:4], min_max_norm))
 data.downsampled_norm_sd<-lapply(data.downsampled_norm[1:4], sd)
 
